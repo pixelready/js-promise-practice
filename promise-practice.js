af2639c95166c9c14e750feb5b6764b0463819ef
pixelready <bizartist@gmail.com>
2022-02-14 17:31:47 -0800

    WIP card display

--- promise-practice.js
+++ promise-practice.js
@@ -1,13 +1,13 @@
 "use strict";
 
 const NUMBERS_API_BASE_URL = "http://numbersapi.com";
+const DECK_API_BASE_URL = "http://deckofcardsapi.com/api";
 const $displayContainer = $("#display-container");
 
 async function getNumFact(num) {
   let response = await axios.get(`${NUMBERS_API_BASE_URL}/${num}/?json`);
   //console.log(response.data.text);
   return response.data.text;
-
 }
 
 async function getNumBatch(nums) {
@@ -28,10 +28,9 @@ async function getAndDisplayNums(...nums) {
   displayNumBatch(numBatch);
 }
 
-async function getFacts(num, numFacts){
-  
+async function getFacts(num, numFacts) {
   const results = [];
-  for(let i = 0; i<numFacts; i++){
+  for (let i = 0; i < numFacts; i++) {
     let fact = await getNumFact(num);
     results.push(fact);
   }
@@ -39,8 +38,8 @@ async function getFacts(num, numFacts){
   return results;
 }
 
-function displayFacts(facts){
-  for (let fact of facts){
+function displayFacts(facts) {
+  for (let fact of facts) {
     //console.log(fact);
     let $displayRow = $('<li class = "display-row">');
     $displayRow.text(`This is your fact: ${fact}`);
@@ -48,12 +47,44 @@ function displayFacts(facts){
   }
 }
 
-async function getAndDisplayFacts(num, numFacts){
+async function getAndDisplayFacts(num, numFacts) {
   const facts = await getFacts(num, numFacts);
-  console.log(facts)
+  console.log(facts);
   displayFacts(facts);
 }
 
 // getAndDisplayNums(24, 42, 89);
 // console.log(getNumFact(42));
-getAndDisplayFacts(7, 4);
+//getAndDisplayFacts(7, 4);
+
+async function getDeck() {
+  let newDeck = await axios.get(`${DECK_API_BASE_URL}/deck/new/shuffle/?deck_count=1`);
+  return newDeck.data.deck_id;
+}
+
+async function drawCardsFromDeck(deckId, num) {
+  let drawnCard = await axios.get(`${DECK_API_BASE_URL}/deck/${deckId}/draw/?count=${num}`);
+  return drawnCard.data.cards;
+}
+
+async function makeDeckAndDraw(num) {
+  let deck = await getDeck();
+  let cards = await drawCardsFromDeck(deck, num);
+  return cards;
+  // console.log(cards);
+  // console.log(`You drew a ${cards[0].value} of ${cards[0].suit}`);
+}
+
+function displayCard(card) {
+  $newCardImage = $(`<img src={card.images.svg}>`);
+  $displayContainer.append($newCardImage);
+}
+
+async function drawCardAndDisplay() {
+  newCards = await makeDeckAndDraw(1);
+  displayCard(newCards[0]);
+}
+
+$("#draw-card").on("click", drawCardAndDisplay);
+
+makeDeckAndDraw(1);
